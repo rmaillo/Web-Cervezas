dff771524388baac35484c545544677251d2f19b
rmaillo <rmaillo@ilerna.com>
2024-05-28 14:19:55 +0200

    Commit_Martes28

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -41,21 +41,62 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // Función botón limpiar filtros
-    document.getElementById('btnClean').addEventListener('click', function(event){
+/*    document.getElementById('btnClean').addEventListener('click', function(event){
         event.preventDefault();
         document.querySelectorAll('.checkboxes').forEach(function(checkbox){
-            checkbox.checked = false;
+           
+            if (checkbox.checked == true) {
+                checkbox.checked = false;
+                // Eliminar clase mostrar filtros
+                document.querySelector('.filter').classList.remove('mostrar');
+                document.querySelector('.filter').classList.add('ocultar');
+            } else {
+                btnCloseResalt = document.querySelector('legend');
+                btnCloseResalt.classList.add('resaltar');
+
+                oo = document.getElementById('oo');
+                oi = document.getElementById('oi');
+                ou = document.getElementById('ou'); 
+                oo.classList.add('resaltar');
+                oi.classList.add('resaltar');
+                ou.classList.add('resaltar');
+                
+                setTimeout(function(){
+                    btnCloseResalt.classList.remove('resaltar');
+                    oo.classList.remove('resaltar');
+                    oi.classList.remove('resaltar');
+                    ou.classList.remove('resaltar');
+                }, 100);
+            }          
         });
-
-        // Renderizar todos los productos
-        renderProducts(allProducts);
-
-        // Eliminar clase mostrar filtros
-        document.querySelector('.filter').classList.remove('mostrar');
-        document.querySelector('.filter').classList.add('ocultar');
-
+*/
+        // Función botón limpiar filtros
+        document.getElementById('btnClean').addEventListener('click', function(event) {
+            event.preventDefault();
+            const checkboxes = document.querySelectorAll('.checkboxes');
+            const filter = document.querySelector('.filter');
+            const btnCloseResalt = document.querySelector('legend');
+        
+            checkboxes.forEach(function(checkbox) {
+                if (checkbox.checked) {
+                    checkbox.checked = false;
+                    filter.classList.remove('mostrar');
+                    filter.classList.add('ocultar');
+                } else {
+                        btnCloseResalt.classList.add('resaltar');
+                        document.querySelectorAll('.filterOptions').forEach(option => option.classList.add('resaltar'));
+        
+                        setTimeout(function() {
+                                btnCloseResalt.classList.remove('resaltar');
+                                document.querySelectorAll('.filterOptions').forEach(option => option.classList.remove('resaltar'));
+                                }, 100);
+                        }
+            });
+            // Renderizar todos los productos
+            renderProducts(allProducts);
+        });
     });
-});
+//});
 
 // Animación filtro
 document.addEventListener('DOMContentLoaded', function(){
@@ -88,6 +129,7 @@ function openFilter() {
 function closeFilter() {
     const btnCloseFilter = document.querySelector('#btnCloseFilter');
     const filter = document.querySelector('.filter');
+    
     btnCloseFilter.addEventListener('click', function() {
         if (filter.classList.contains('mostrar')) {
             filter.classList.remove('mostrar');
@@ -126,11 +168,13 @@ function filterChecked() {
         }
     });
     btnClean.addEventListener('click', function() {
+
         check1.checked = false;
         check2.checked = false;
         check3.checked = false;
         check1.disabled = false;
         check2.disabled = false;
         check3.disabled = false;
+        
     });
 }
